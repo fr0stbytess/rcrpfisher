b00c7c898e462fc01ce365b52892ca1a15079c45
frostyworks <35506101+frostyworks@users.noreply.github.com>
2019-11-16 15:25:58 +0100

    Added "rcrpfisher.py"

---
+++ rcrpfisher.py
@@ -0,0 +1,30 @@
+import sys
+import time
+import pyautogui as fisher
+
+
+fishing = True
+
+def Fishing():
+    tries = 5
+    try:
+        while fishing:
+            if tries == 0:
+                tries = 5
+                CollectFish()
+            fisher.press("T")
+            fisher.typewrite("/fish")
+            fisher.press("enter")
+            tries -= 1
+            time.sleep(17) # cooldown so shit won't spam out and alert admins
+
+    except KeyboardInterrupt:
+        sys.exit()
+        print("Stopped.")
+
+def CollectFish():
+    fisher.press("T")
+    fisher.typewrite("/fcrate store all") # stores all our fishes so we can fish in peace
+    fisher.press("enter")
+
+Fishing()
