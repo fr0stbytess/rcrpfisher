0aa245326bb6b4d973077e51207a8a15b58b052e
frostyworks <35506101+frostyworks@users.noreply.github.com>
2020-01-24 19:18:46 +0100

    respect pep8

--- rcrpfisher.py
+++ rcrpfisher.py
@@ -2,8 +2,8 @@ import sys
 import time
 import pyautogui as fisher
 
-
 fishing = True
+
 
 def Fishing():
     tries = 5
@@ -16,15 +16,16 @@ def Fishing():
             fisher.typewrite("/fish")
             fisher.press("enter")
             tries -= 1
-            time.sleep(17) # cooldown so shit won't spam out and alert admins
+            time.sleep(17)  # cooldown so shit won't spam out and alert admins
 
     except KeyboardInterrupt:
         sys.exit()
         print("Stopped.")
 
+
 def CollectFish():
     fisher.press("T")
-    fisher.typewrite("/fcrate store all") # stores all our fishes so we can fish in peace
+    fisher.typewrite("/fcrate store all")  # stores fishes for peace
     fisher.press("enter")
 
 Fishing()
